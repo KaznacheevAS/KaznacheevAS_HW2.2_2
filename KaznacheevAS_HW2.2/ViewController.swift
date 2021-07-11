200872ade7c59086237c55f3d957cc2bcf3ea0e5
Kaznacheev <antkaznacheev@gmail.ru>
2021-07-11 12:11:26 +0300

    Приложение в котором можно менять цвет заливки вью

--- KaznacheevAS_HW2.2/ViewController.swift
+++ KaznacheevAS_HW2.2/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Outlet
+    @IBOutlet weak var bacgraundList: UIView!
+    
+    @IBOutlet weak var rgbReg: UISlider!
+    @IBOutlet weak var rgbGreen: UISlider!
+    @IBOutlet weak var rgbBlue: UISlider!
+    
+    @IBOutlet weak var labelRed: UILabel!
+    @IBOutlet weak var labelGreen: UILabel!
+    @IBOutlet weak var labelBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        bacgraundList.layer.cornerRadius = 10
+        
+        rgbReg.minimumTrackTintColor = .red
+        rgbGreen.minimumTrackTintColor = .green
+        rgbBlue.minimumTrackTintColor = .blue
+        
+        bacgraundList.backgroundColor = UIColor(
+            red: CGFloat(rgbReg.value),
+            green: CGFloat(rgbGreen.value),
+            blue: CGFloat(rgbBlue.value),
+            alpha: 1)
+        
+        labelRed.text = "Red \(String(format: "%.2f", rgbReg.value))"
+        labelGreen.text = "Green \(String(format: "%.2f", rgbGreen.value))"
+        labelBlue.text = "Blue \(String(format: "%.2f", rgbBlue.value))"
+
     }
 
-
+    //MARK: Action
+    @IBAction func colorSlider() {
+        bacgraundList.backgroundColor = UIColor(
+            red: CGFloat(rgbReg.value),
+            green: CGFloat(rgbGreen.value),
+            blue: CGFloat(rgbBlue.value),
+            alpha: 1
+        )
+        
+        labelRed.text = "Red \(String(format: "%.2f", rgbReg.value))"
+        labelGreen.text = "Green \(String(format: "%.2f", rgbGreen.value))"
+        labelBlue.text = "Blue \(String(format: "%.2f", rgbBlue.value))"
+        
+    }
+    
 }
 
